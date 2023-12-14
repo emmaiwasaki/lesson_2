d472274d5e343e8519f03f3a00878974c8914b4b
emmaiwasaki <emmaiwasaki@berkeley.edu>
2023-12-13 22:02:41 -0800

    fixed line endings

--- practice/calculator.js
+++ practice/calculator.js
@@ -1,31 +1,30 @@
 const readline = require("readline-sync");
+console.log("welcome to Calculator!");
 
-console.log("Welcome to Calculator!");
-
-console.log("What's the first number?");
+console.log("what's the first number?");
 let number1 = readline.question();
 
-console.log("What's the second number?");
+console.log("what's the second number?");
 let number2 = readline.question();
 
 console.log(
-  "What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide"
+  "what operation would you liek to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide"
 );
 let operation = readline.question();
 
 let output;
-if (operation === "1") {
+if (operation === '1') {
   // '1' represents addition
   output = Number(number1) + Number(number2);
-} else if (operation === "2") {
+} else if (operation === '2') {
   // '2' represents subtraction
   output = Number(number1) - Number(number2);
-} else if (operation === "3") {
-  // 3 represents multiplication
+} else if (operation === '3') {
+  // '3' represents multiplication
   output = Number(number1) * Number(number2);
-} else if (operation === "4") {
-  // 4 represents division
+} else if (operation === '4') {
+  // '4' represents division
   output = Number(number1) / Number(number2);
 }
 
-console.log(`The result is: ${output}`);
+console.log(`the result is: ${output}`);
